e9682d5973bde04d0ead17e02dbe6428745363f1
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2023-10-17 18:42:49 -0700

    Seprated the navbar in nav.php.

---
+++ views/partials/nav.php
@@ -0,0 +1,15 @@
+<header>
+        <div class="container">
+        <!-- <div class="logo">
+                <img src="images/gym logo.png" alt="Gym Logo">
+            </div> -->
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about.php">About</a></li>
+                    <li><a href="/Registration.php">Registration</a></li>
+                    <li><a href="/contact.php">Contact us</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
