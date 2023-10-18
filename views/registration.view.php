8adfff62bec0368210fa8a83d44f83d4438df267
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2023-10-17 21:57:44 -0700

    Registration page content add to the project.

--- views/registration.view.php
+++ views/registration.view.php
@@ -8,5 +8,22 @@
                
             </div>
         </section>
+        <section class="registration-form">
+        <div class="container">
+            <h2>Register Now</h2>
+            <form action="#" method="post">
+                <label for="name">Full Name:</label><br>
+                <input type="text" id="name" name="name" required><br>
+                
+                <label for="email">Email:</label><br>
+                <input type="email" id="email" name="email" required><br>
+
+                <label for="password">Password:</label><br>
+                <input type="password" id="password" name="password" required><br>
+
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+    </section>
         <?php require "partials/footer.php";
 ?>
