89a2e22fafaa53b3e3ce68a7288441530161e1ba
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2023-10-17 18:30:11 -0700

    New contact page add and link that to contact.view.php.

---
+++ contact.view.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fitness Gym</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+        <div class="container">
+        <!-- <div class="logo">
+                <img src="images/gym logo.png" alt="Gym Logo">
+            </div> -->
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about.php">About</a></li>
+                    <li><a href="/Registration.php">Registration</a></li>
+                    <li><a href="/contact.php">Contact us</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <section class="hero">
+            <div class="container">
+                <h2>Wlecome to contact Page</h2>
+               
+            </div>
+        </section>
+        <footer>
+        <div class="container">
+            <p>&copy; 2023 Gym Website. All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+</html>
