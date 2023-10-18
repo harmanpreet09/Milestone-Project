a991effec94ead85193680bbc02ce8eee29be568
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2023-10-17 17:17:54 -0700

    Nav bar add in the html code.

---
+++ index.view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fitness Gym</title>
+</head>
+<body>
+<header>
+        <div class="container">
+        <!-- <div class="logo">
+                <img src="images/gym logo.png" alt="Gym Logo">
+            </div> -->
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about.php">About</a></li>
+                    <li><a href="/Registration.php">Registration</a></li>
+                    <li><a href="/contact.php">Contact us</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
