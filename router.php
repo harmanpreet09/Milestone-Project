826ead42feb33c36240b9676436714d9deb8c080
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2023-10-17 19:44:43 -0700

    Updated the routes array in routes.php.

--- router.php
+++ router.php
@@ -1,2 +1,8 @@
 <?php
 $uri =parse_url ( $_SERVER['REQUEST_URI']) ['path'];
+$routes= [
+    '/'=> 'controllers/index.php',
+    '/about'=> 'controllers/about.php',
+    '/contact'=> 'controllers/contact.php',
+    '/contact'=> 'controllers/registration.php',
+];
