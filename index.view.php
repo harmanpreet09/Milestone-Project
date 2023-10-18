918f7a69e5d04cad0d7358138f7dcd8d1c94fb94
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2023-10-17 17:49:10 -0700

    Hero section add to home page.

--- index.view.php
+++ index.view.php
@@ -22,5 +22,12 @@
             </nav>
         </div>
     </header>
+    <section class="hero">
+            <div class="container">
+                <h2>Your Fitness Journey Begins Here</h2>
+                <p>Join our gym and start your path to a healthier lifestyle.</p>
+                <a href="#" class="cta-button">Get Started</a>
+            </div>
+        </section>
 </body>
 </html>
