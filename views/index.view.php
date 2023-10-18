3166b1f24776de0f70c645ce09092dfba23ea132
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2023-10-17 21:15:16 -0700

    Style add to the services section.

--- views/index.view.php
+++ views/index.view.php
@@ -10,5 +10,34 @@
                 <a href="#" class="cta-button">Get Started</a>
             </div>
         </section>
+        <section class="services">
+            <div class="container center-content">
+            <h2>Our Best Services</h2>
+                <div class="best-service">
+                
+                <div class="service">
+                    <img src="images/yoga.jpg" alt="Service 1">
+                    <div class="service-info">
+                        <h3>Personal Training</h3>
+                        <p>One-on-one training with our experienced trainers to achieve your fitness goals.</p>
+                    </div>
+                </div>
+                <div class="service">
+                    <img src="images/yoga.jpg" alt="Service 2">
+                    <div class="service-info">
+                        <h3>Group Classes</h3>
+                        <p>Engage in a variety of group fitness classes for all fitness levels and preferences.</p>
+                    </div>
+                </div>
+                <div class="service">
+                    <img src="images/yoga.jpg" alt="Service 3">
+                    <div class="service-info">
+                        <h3>Nutritional Guidance</h3>
+                        <p>Customized nutrition plans and guidance to complement your fitness routine.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </section>
         <?php require "partials/footer.php";
 ?>
