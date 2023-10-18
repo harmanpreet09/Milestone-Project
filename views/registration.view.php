fdfa9f76cc214760fcc18b6f0f6344f20e06406c
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2023-10-17 18:44:04 -0700

    Replace the nav bar with the require link of nav.php.

--- views/registration.view.php
+++ views/registration.view.php
@@ -7,21 +7,8 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<header>
-        <div class="container">
-        <!-- <div class="logo">
-                <img src="images/gym logo.png" alt="Gym Logo">
-            </div> -->
-            <nav>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about.php">About</a></li>
-                    <li><a href="/Registration.php">Registration</a></li>
-                    <li><a href="/contact.php">Contact us</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+<?php require "partials/nav.php";
+?>
     <section class="hero">
             <div class="container">
                 <h2>Wlecome to Registration</h2>
