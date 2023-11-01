ea7a6787e662b4a5e2c5d1d2ebd522797a7e4e6a
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2023-10-31 22:52:04 -0700

    Change the heading size in the about page main text.

--- views/contact.view.php
+++ views/contact.view.php
@@ -2,11 +2,40 @@
 ?>
 <?php require "partials/nav.php";
 ?>
-    <section class="hero">
+    <section class="hero_2 hero">
             <div class="container">
-                <h2>Wlecome to contact Page</h2>
+                <h2>Welcome to contact Page</h2>
+
                
             </div>
         </section>
+        <section class="contact-content">
+        <div class="container">
+            <h2>Contact Us</h2>
+            <p>Have questions or feedback? Contact us using the form below or the provided contact information.</p>
+
+            <div class="contact-form">
+                <h3>Contact Form</h3>
+                <form action="#" method="post">
+                    <label for="name">Full Name:</label><br>
+                    <input type="text" id="name" name="name" required><br>
+
+                    <label for="email">Email:</label><br>
+                    <input type="email" id="email" name="email" required><br>
+
+                    <label for="message">Message:</label><br>
+                    <textarea id="message" name="message" rows="5" required></textarea><br>
+
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+
+            <div class="contact-info">
+                <h3>Contact Information</h3>
+                <p><strong>Email:</strong> info@example.com</p>
+                <p><strong>Phone:</strong> +123 456 789</p>
+            </div>
+        </div>
+    </section>
         <?php require "partials/footer.php";
 ?>
