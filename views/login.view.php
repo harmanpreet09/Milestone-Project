b79618e9983a898de54686e98621d2c5947c10eb
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2023-11-07 19:37:57 -0800

    New login page create.

---
+++ views/login.view.php
@@ -0,0 +1,25 @@
+
+
+<?php require "partials/head.php";
+?>
+<?php require "partials/nav.php";
+?>
+ 
+        <section class="registration-form">
+        <div class="container">
+            <h2>Register Now</h2>
+            <form action="#" method="post">
+               
+                
+                <label for="username">usename</label><br>
+                <input type="text" id="username" name="username" required><br>
+
+                <label for="password">Password:</label><br>
+                <input type="password" id="password" name="password" required><br>
+
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+    </section>
+        <?php require "partials/footer.php";
+?>
